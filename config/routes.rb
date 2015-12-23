a6c82926914c5064c97b0771fa7cba5e3f9ed4ff
zorganon <dan.the.hudson@gmail.com>
2015-12-23 04:41:20 +0000

    added dynamic linking to the nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
+  get 'static_pages/index'
+
+  get 'static_pages/index'
+  
+  get 'static_pages/philosophy'
+  
   get 'static_pages/contact'
-
-  get 'static_pages/index'
-
-  get 'static_pages/index'
   
   resources :orders, only: [:index, :show, :create, :destroy]
 
@@ -18,7 +20,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  root 'static_pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
