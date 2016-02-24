49dbc4fe81be89fdd60d1e754a179baac18b7cff
zorganon <dan.the.hudson@gmail.com>
2016-02-24 21:58:44 +0000

    added ajax to update comments and refresh rating without re-loading page

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,11 +1,16 @@
-$(document).on('ready page:load', function(){
-  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+function ratey (){
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
   $('.rated').raty({ path: '/assets',
     readOnly: true,
     score: function() {
       return $(this).attr('data-score');
     }
   });
+}
+
+$(document).on('ready page:load', function(){
+  ratey();
   //this bit is for the zoom feature
   $('.img-zoom').elevateZoom();
+//  $("#new_comment").on("submit", reload);
 });
